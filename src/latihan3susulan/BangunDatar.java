65602d7993acf0741497276428197d7ec5b2d8f7
Kholik01 <abdir200803@gmail.com>
2025-06-24 17:05:47 +0700

    initial commit

---
+++ src/latihan3susulan/BangunDatar.java
@@ -0,0 +1,12 @@
+package latihan3susulan;
+
+public class BangunDatar {
+    // Nama: Abdi Rahman Kholik
+    // NPM: 2310010632
+    // Kelas: 4A
+
+    public static void main(String[] args) {
+        System.out.println("Ini adalah class BangunDatar");
+    }
+}
+
